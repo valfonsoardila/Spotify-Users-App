351287c0e219ab63384f69c00d5710bab9faa72e
VictorArdila <valfonsoardila@unicesar.edu.co>
2024-03-12 17:55:36 -0500

    Animacion de la intro avanzada

---
+++ src/components/anim/AnimationIntro.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { resources } from "../../assets/resources";
+import Particles from "./Particles";
+import "./AnimationIntro.css";
+
+const AnimationIntro = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+      className="intro-container"
+    >
+      <Particles id="particles" />
+      <img src={resources.logo} alt="logo de Spotify" />
+      <h1 style={{color:'#363636', marginTop:'1%'}}>Millions of users.</h1>
+      <h1 style={{marginTop:'-2%'}}>Tuning with Spotify</h1>
+      <p>Disfruta la mejor comunidad sonora y vibra con sus historias</p>
+    </motion.div>
+  );
+};
+
+export default AnimationIntro;
